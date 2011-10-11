9753690cd06ee63a017c20b212c2b39cdf06eb69
Artur Siery <dotintegral@gmail.com>
2011-10-11 21:09:49 +0200

    Test commands removed

--- IrcBot/src/main/java/org/siery/irc/command/utils/CommandFactory.java
+++ IrcBot/src/main/java/org/siery/irc/command/utils/CommandFactory.java
@@ -5,10 +5,13 @@ import java.util.ArrayList;
 import org.siery.irc.command.CommandsCommand;
 import org.siery.irc.command.HelpCommand;
 import org.siery.irc.command.Magic8BallCommand;
+import org.siery.irc.command.MoreCommand;
 import org.siery.irc.command.NickHuntCommand;
 import org.siery.irc.command.PonyCommand;
+import org.siery.irc.command.ReadCommand;
 import org.siery.irc.command.ReloadCommand;
 import org.siery.irc.command.TestCommand;
+import org.siery.irc.command.WolframAlphaCommand;
 import org.siery.irc.command.abstracts.Command;
 
 public class CommandFactory {
@@ -18,12 +21,13 @@ public class CommandFactory {
 		ArrayList<Command> commands = new ArrayList<Command>();
 
 		commands.add(new CommandsCommand());
-		commands.add(new TestCommand());
-		commands.add(new PonyCommand());
 		commands.add(new NickHuntCommand());
 		commands.add(new ReloadCommand());
 		commands.add(new HelpCommand());
 		commands.add(new Magic8BallCommand());
+		commands.add(new WolframAlphaCommand());
+		commands.add(new MoreCommand());
+		commands.add(new ReadCommand());
 
 		return commands;
 	}
